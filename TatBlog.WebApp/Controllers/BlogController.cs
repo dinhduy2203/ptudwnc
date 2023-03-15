f7a4187265a547f579fb5b894c150dab0e5174f9
duy <dinhduy0322@gmail.com>
2023-03-15 07:25:25 +0700

    q

--- TatBlog.WebApp/Controllers/BlogController.cs
+++ TatBlog.WebApp/Controllers/BlogController.cs
@@ -5,30 +5,25 @@ namespace TatBlog.WebApp.Controllers
 {
     public class BlogController : Controller
     {
-
         private readonly IBlogRepository _blogRepository;
-
         public BlogController(IBlogRepository blogRepository)
         {
             _blogRepository = blogRepository;
         }
         public async Task<IActionResult> Index(
-            [FromQuery(Name ="p")] int pageNumber =1,
-            [FromQuery(Name ="ps")] int pageSize = 10)
-        
+            [FromQuery(Name = "k")] string keyword = "",
+            [FromQuery(Name = "p")] int pageNumber = 1,
+            [FromQuery(Name = "ps")] int pageSize = 10)
         {
             var postQuery = new PostQuery()
             {
-                PublisheOnly = true
+                PublishedOnly = true,
+                KeyWord = keyword
             };
-
-            var postsList = await _blogRepository
-                .GetPagedPostAsync(postQuery, pageNumber, pageSize);
-                ViewBar.PostQuery = postQuery;
+            var postsList = await _blogRepository.GetPagedPostsAsync(postQuery, pageNumber, pageSize);
+            ViewBag.PostQuery = postQuery;
             return View(postsList);
         }
-        
-    
         public IActionResult About() => View();
         public IActionResult Contact() => View();
         public IActionResult Rss() => Content("Nội dung sẽ được cập nhật");
